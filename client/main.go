591b8e812e02e3ab4295537582c21048a4baa27e
Marten Seemann <martenseemann@gmail.com>
2024-12-16 00:58:08 +0800

    dial a raw connection on the same 4-tuple as the holepunched libp2p conn

--- client/main.go
+++ client/main.go
@@ -10,10 +10,12 @@ import (
 	"time"
 
 	"github.com/libp2p/go-libp2p"
-	"github.com/libp2p/go-libp2p-kad-dht"
+	dht "github.com/libp2p/go-libp2p-kad-dht"
 	"github.com/libp2p/go-libp2p/core/event"
+	"github.com/libp2p/go-libp2p/core/network"
 	"github.com/libp2p/go-libp2p/core/peer"
 	libp2pquic "github.com/libp2p/go-libp2p/p2p/transport/quic"
+	"github.com/libp2p/go-libp2p/p2p/transport/quicreuse"
 	ma "github.com/multiformats/go-multiaddr"
 	mafmt "github.com/multiformats/go-multiaddr-fmt"
 	manet "github.com/multiformats/go-multiaddr/net"
@@ -37,18 +39,35 @@ func main() {
 }
 
 func runClient(target peer.ID) (quic.Connection, error) {
+	udpConn, err := net.ListenUDP("udp4", &net.UDPAddr{IP: net.IPv4zero, Port: 0})
+	if err != nil {
+		return nil, fmt.Errorf("failed to create UDP listener: %w", err)
+	}
+	tr := &quic.Transport{Conn: udpConn}
+	newReuse := func(statelessResetKey quic.StatelessResetKey, tokenGeneratorKey quic.TokenGeneratorKey) (*quicreuse.ConnManager, error) {
+		reuse, err := quicreuse.NewConnManager(statelessResetKey, tokenGeneratorKey)
+		if err != nil {
+			return nil, fmt.Errorf("failed to create reuse: %w", err)
+		}
+		if err := reuse.AddTransport("udp4", &wrappedQUICTransport{tr}, udpConn); err != nil {
+			return nil, fmt.Errorf("failed to add transport to reuse: %w", err)
+		}
+		return reuse, nil
+	}
 	h, err := libp2p.New(
 		libp2p.Transport(libp2pquic.NewTransport),
-		libp2p.ListenAddrStrings("/ip4/0.0.0.0/udp/0/quic-v1"),
+		libp2p.ListenAddrStrings(fmt.Sprintf("/ip4/0.0.0.0/udp/%d/quic-v1", udpConn.LocalAddr().(*net.UDPAddr).Port)),
 		libp2p.EnableHolePunching(),
+		libp2p.QUICReuse(newReuse),
 	)
 	if err != nil {
 		return nil, err
 	}
 	defer h.Close()
+	log.Println("I am:", h.ID())
 
+	connected := make(chan struct{}, 1)
 	go func() {
-		fmt.Println("subscribing to peer connectedness changed event")
 		sub, err := h.EventBus().Subscribe(new(event.EvtPeerConnectednessChanged))
 		if err != nil {
 			log.Fatal("failed to subscribe to peer connectedness changed event: ", err)
@@ -57,7 +76,14 @@ func runClient(target peer.ID) (quic.Connection, error) {
 		for ev := range sub.Out() {
 			e := ev.(event.EvtPeerConnectednessChanged)
 			if e.Peer == target {
-				fmt.Println("peer connectedness changed:", e.Peer, e.Connectedness)
+				msg := fmt.Sprintf("peer connectedness changed: %s\n", e.Connectedness)
+				for _, c := range h.Network().ConnsToPeer(target) {
+					msg += fmt.Sprintf("\t%s <-> %s\n", c.LocalMultiaddr(), c.RemoteMultiaddr())
+				}
+				log.Println(msg)
+				if e.Connectedness == network.Connected {
+					connected <- struct{}{}
+				}
 			}
 		}
 	}()
@@ -95,33 +121,83 @@ func runClient(target peer.ID) (quic.Connection, error) {
 	var udpAddr *net.UDPAddr
 	for _, a := range h.Peerstore().Addrs(target) {
 		if manet.IsPublicAddr(a) && isQUICAddr(a) {
-			netAddr, err := manet.ToNetAddr(a)
+			udpAddr, err = quicAddrToNetAddr(a)
 			if err != nil {
-				return nil, err
+				return nil, fmt.Errorf("failed to convert multiaddr to net.UDPAddr: %w", err)
 			}
-			udpAddr = netAddr.(*net.UDPAddr)
+			break
 		}
 	}
 	if udpAddr != nil {
-		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-		defer cancel()
-		conn, err := quic.DialAddr(ctx, udpAddr.String(), &tls.Config{InsecureSkipVerify: true}, nil)
-		if err != nil {
-			return nil, fmt.Errorf("failed to dial QUIC address: %w", err)
-		}
-		return conn, nil
+		return dialQUIC(tr, udpAddr)
 	}
 
 	// 2nd step: connect to the target via a relay address
 	if err := h.Connect(context.Background(), ai); err != nil {
 		return nil, fmt.Errorf("failed to connect to peer: %w", err)
 	}
-	for _, c := range h.Network().ConnsToPeer(target) {
-		log.Printf("connected to %s via %s\n", c.RemotePeer(), c.RemoteMultiaddr())
+
+	select {
+	case <-connected:
+	case <-time.After(5 * time.Second):
+		return nil, fmt.Errorf("timed out waiting for direct (e.g. hole-punched) connection")
 	}
-	select {}
+
+	var directAddr *net.UDPAddr
+	for _, c := range h.Network().ConnsToPeer(target) {
+		if a := c.RemoteMultiaddr(); isQUICAddr(a) {
+			directAddr, err = quicAddrToNetAddr(a)
+			if err != nil {
+				return nil, fmt.Errorf("failed to convert multiaddr to net.UDPAddr: %w", err)
+			}
+			break
+		}
+	}
+	if directAddr == nil {
+		return nil, fmt.Errorf("failed to find a direct QUIC address for peer %s after hole punching", target)
+	}
+	log.Printf("dialing QUIC address: %s", directAddr)
+	return dialQUIC(tr, directAddr)
+}
+
+func dialQUIC(tr *quic.Transport, addr *net.UDPAddr) (quic.Connection, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	conn, err := tr.Dial(
+		ctx,
+		addr,
+		&tls.Config{
+			InsecureSkipVerify: true,
+			NextProtos:         []string{"raw"},
+		},
+		nil,
+	)
+	if err != nil {
+		return nil, fmt.Errorf("failed to dial QUIC address: %w", err)
+	}
+	return conn, nil
 }
 
 func isQUICAddr(a ma.Multiaddr) bool {
 	return mafmt.And(mafmt.IP, mafmt.Base(ma.P_UDP), mafmt.Base(ma.P_QUIC_V1)).Matches(a)
+}
+
+func quicAddrToNetAddr(a ma.Multiaddr) (*net.UDPAddr, error) {
+	first, _ := ma.SplitFunc(a, func(c ma.Component) bool { return c.Protocol().Code == ma.P_QUIC_V1 })
+	if first == nil {
+		return nil, fmt.Errorf("no QUIC address found in multiaddr")
+	}
+	netAddr, err := manet.ToNetAddr(first)
+	if err != nil {
+		return nil, fmt.Errorf("failed to convert multiaddr to net.Addr: %w", err)
+	}
+	return netAddr.(*net.UDPAddr), nil
+}
+
+type wrappedQUICTransport struct {
+	*quic.Transport
+}
+
+func (t *wrappedQUICTransport) Listen(tlsConf *tls.Config, conf *quic.Config) (quicreuse.QUICListener, error) {
+	return t.Transport.Listen(tlsConf, conf)
 }
